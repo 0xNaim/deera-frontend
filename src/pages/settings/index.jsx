d30f9a95616b1f885aec5a18dac0e0dca60e2226
Naim Islam <naimislam3545@gmail.com>
2022-12-27 23:09:21 +0600

    feat: default theme implemented

---
+++ src/pages/settings/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@mui/material';
+import Layout from '../../components/Layout';
+
+const Settings = () => (
+  <Layout>
+    <Button variant="contained">Test</Button>
+  </Layout>
+);
+
+export default Settings;
---
+++ src/theme/theme.jsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#F2A515',
+    },
+  },
+});
+
+export default theme;
