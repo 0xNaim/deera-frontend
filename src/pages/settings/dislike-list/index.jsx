4478c5e5ac80fa77e0b38c07fc13fd76245a2920
Naim Islam <naimislam3545@gmail.com>
2022-12-29 01:56:53 +0600

    feat(settings): settings/dislike-list page design complete

---
+++ src/pages/settings/dislike-list/index.jsx
@@ -0,0 +1,108 @@
+/* eslint-disable comma-dangle */
+import DoneIcon from '@mui/icons-material/Done';
+import { Box, Button, Chip, Container, Typography } from '@mui/material';
+import { useState } from 'react';
+import Layout from '../../../components/Layout';
+import SettingsHead from '../head';
+import styles from './dislike-list.module.scss';
+
+const initialIngredients = [
+  'Chicken',
+  'Crustaceans',
+  'Meat',
+  'Fish',
+  'Spicy',
+  'Habash',
+  'Nuts',
+  'Beans',
+  'Peanuts',
+  'Soya',
+  'Milk products',
+  'Gluten',
+  'Psyllium cortex',
+];
+
+const DislikeList = () => {
+  const [ingredients] = useState(initialIngredients);
+  const [selectedIngredients, setSelectedIngredients] = useState(['Naim', 'Onoy']);
+
+  // Handle select ingredients
+  const handleSelectIngredient = (ingredient) => {
+    // Find ingredient if already exist or not
+    const findIngredient = selectedIngredients.find(
+      (selectedIngredient) => selectedIngredient === ingredient
+    );
+
+    if (!findIngredient) {
+      // If ingredient not exist then push ingredient in selectedIngredients list
+      setSelectedIngredients((prevIngredients) => [...prevIngredients, ingredient]);
+    } else {
+      // If ingredient already exist then remove the ingredient
+      const filteredIngredients = selectedIngredients.filter(
+        (selectedIngredient) => selectedIngredient !== ingredient
+      );
+      setSelectedIngredients(filteredIngredients);
+    }
+  };
+
+  // Get ingredients
+  const getIngredient = (value) => {
+    // Find ingredient based on selected ingredient
+    const ingredient = selectedIngredients.find(
+      (selectedIngredient) => selectedIngredient === value
+    );
+    return ingredient;
+  };
+
+  return (
+    <Layout>
+      <Container>
+        <Box className={styles.content__wrapper}>
+          <SettingsHead heading="Prohibited ingredients" />
+
+          <Box className={styles.content}>
+            <Box className={styles.description}>
+              <Typography color="secondary">
+                Please specify the ingredients that you would like to avoid in choosing meals!
+              </Typography>
+            </Box>
+
+            <Box className={styles.ingredients__wrapper}>
+              {ingredients.map((ingredient) => (
+                <Chip
+                  className={
+                    getIngredient(ingredient) === ingredient
+                      ? `${styles.ingredient} ${styles['active-ingredient']}`
+                      : `${styles.ingredient}`
+                  }
+                  key={ingredient}
+                  onClick={() => handleSelectIngredient(ingredient)}
+                  label={ingredient}
+                  icon={getIngredient(ingredient) === ingredient && <DoneIcon color="primary" />}
+                  variant="outlined"
+                />
+              ))}
+            </Box>
+
+            <Box className={styles.button__group}>
+              <Button
+                className={styles['other_gradient--btn']}
+                color="secondary"
+                variant="outlined"
+                fullWidth
+              >
+                Other Ingredients
+              </Button>
+
+              <Button className={styles.submit__btn} variant="contained" fullWidth>
+                Submit
+              </Button>
+            </Box>
+          </Box>
+        </Box>
+      </Container>
+    </Layout>
+  );
+};
+
+export default DislikeList;
