ee6e7f04d3cfdc644bc9d64a4370ff3ac3d0b8f3
sonjoybarmon <sonjoybarmon19@gmail.com>
2023-01-17 05:11:17 +0600

    feat: a condition has been added,if ar is in your cookies, then you cannot route with en

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { ThemeProvider } from '@mui/material';
 import { appWithTranslation } from 'next-i18next';
-import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-horizontal-strip-datepicker/dist/ReactHorizontalDatePicker.css';
 import * as Cookies from '../hooks/cookies';
@@ -13,6 +15,41 @@ const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
     document.body.dir = currentLanguageCode === 'en' ? 'ltr' : 'rtl';
   }, [currentLanguageCode]);
+
+  const router = useRouter();
+  const [language, setLanguage] = useState('en');
+  useEffect(() => {
+    const storedLocale = Cookies.getLanguage();
+    if (storedLocale) {
+      setLanguage(storedLocale);
+    }
+  }, [language]);
+
+  function rerouteLocale(wantedLocale) {
+    if (router.locale) {
+      if (router.locale !== wantedLocale) {
+        router.replace(router.asPath, router.asPath, { locale: wantedLocale });
+      }
+    } else if (router.defaultLocale && router.defaultLocale !== wantedLocale) {
+      router.replace(router.asPath, router.asPath, { locale: wantedLocale });
+    }
+  }
+
+  useEffect(() => {
+    const storedLocale = Cookies.getLanguage();
+    if (!storedLocale) Cookies.setLanguage('en');
+  }, []);
+
+  useEffect(() => {
+    const storedLocale = Cookies.getLanguage();
+
+    if (storedLocale) {
+      rerouteLocale(storedLocale);
+    } else if (router.locale && (router.locale === 'it' || router.locale === 'en')) {
+      Cookies.setLanguage(router.locale);
+    }
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />
