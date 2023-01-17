4e503416fbb3bb1f8de0b753ed803436e75c7469
sonjoybarmon <sonjoybarmon19@gmail.com>
2023-01-18 00:21:26 +0600

    feat: client settings page multi language added

--- src/pages/settings/dislike-list/index.jsx
+++ src/pages/settings/dislike-list/index.jsx
@@ -1,118 +1,19 @@
 /* eslint-disable comma-dangle */
-import DoneIcon from '@mui/icons-material/Done';
-import { Box, Button, Chip, Container, Typography } from '@mui/material';
-import Link from 'next/link';
-import { useState } from 'react';
-import SettingsHead from '../../../components/Client/head';
-import Navbar from '../../../components/Layout/Navbar/Navbar';
-import SettingsLayout from '../settings-layout';
-import styles from './dislike-list.module.scss';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import dynamic from 'next/dynamic';
 
-const initialIngredients = [
-  'Chicken',
-  'Crustaceans',
-  'Meat',
-  'Fish',
-  'Spicy',
-  'Habash',
-  'Nuts',
-  'Beans',
-  'Peanuts',
-  'Soya',
-  'Milk products',
-  'Gluten',
-  'Psyllium cortex',
-];
+const DislikeList = dynamic(
+  () => import('../../../components/Client/ProhibitedIngredients/ProhibitedIngredients'),
+  { ssr: false }
+);
 
-const DislikeList = () => {
-  const [ingredients] = useState(initialIngredients);
-  const [selectedIngredients, setSelectedIngredients] = useState([]);
+const DislikeListPage = () => (
+  <div>
+    <DislikeList />
+  </div>
+);
 
-  // Handle select ingredients
-  const handleSelectIngredient = (ingredient) => {
-    // Find ingredient if already exist or not
-    const findIngredient = selectedIngredients.find(
-      (selectedIngredient) => selectedIngredient === ingredient
-    );
-
-    if (!findIngredient) {
-      // If ingredient not exist then push ingredient in selectedIngredients list
-      setSelectedIngredients((prevIngredients) => [...prevIngredients, ingredient]);
-    } else {
-      // If ingredient already exist then remove the ingredient
-      const filteredIngredients = selectedIngredients.filter(
-        (selectedIngredient) => selectedIngredient !== ingredient
-      );
-      setSelectedIngredients(filteredIngredients);
-    }
-  };
-
-  // Get ingredients
-  const getIngredient = (value) => {
-    // Find ingredient based on selected ingredient
-    const ingredient = selectedIngredients.find(
-      (selectedIngredient) => selectedIngredient === value
-    );
-    return ingredient;
-  };
-
-  return (
-    <>
-      <Box className={styles.mobile__menu}>
-        <Navbar />
-      </Box>
-
-      <SettingsLayout>
-        <SettingsHead heading="Prohibited ingredients" />
-
-        <Container>
-          <Box className={styles.content__wrapper}>
-            <Box className={styles.content}>
-              <Box className={styles.description}>
-                <Typography color="secondary">
-                  Please specify the ingredients that you would like to avoid in choosing meals!
-                </Typography>
-              </Box>
-
-              <Box className={styles.ingredients__wrapper}>
-                {ingredients.map((ingredient) => (
-                  <Chip
-                    className={
-                      getIngredient(ingredient) === ingredient
-                        ? `${styles.ingredient} ${styles['active-ingredient']}`
-                        : `${styles.ingredient}`
-                    }
-                    key={ingredient}
-                    onClick={() => handleSelectIngredient(ingredient)}
-                    label={ingredient}
-                    icon={getIngredient(ingredient) === ingredient && <DoneIcon color="primary" />}
-                    variant="outlined"
-                  />
-                ))}
-              </Box>
-
-              <Box className={styles.button__group}>
-                <Link href="/settings/add-new-dislike-item" style={{ textDecoration: 'none' }}>
-                  <Button
-                    className={styles['other_gradient--btn']}
-                    color="secondary"
-                    variant="outlined"
-                    fullWidth
-                  >
-                    Other Ingredients
-                  </Button>
-                </Link>
-
-                <Button className={styles.submit__btn} variant="contained" fullWidth>
-                  Submit
-                </Button>
-              </Box>
-            </Box>
-          </Box>
-        </Container>
-      </SettingsLayout>
-    </>
-  );
-};
-
-export default DislikeList;
+export async function getStaticProps({ locale }) {
+  return { props: { ...(await serverSideTranslations(locale, ['ProhibitedIngredients'])) } };
+}
+export default DislikeListPage;
