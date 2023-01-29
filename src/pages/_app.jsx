32e94c79ea0e088cc353e43f7514bbf70735a4a6
Omar Faruq <naimislam3545@gmail.com>
2023-01-29 20:57:50 +0600

    feat: jsconfig.json file added

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,7 @@ import '@styles/reset.scss';
 import theme from '@theme/theme';
 import { appWithTranslation } from 'next-i18next';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-horizontal-strip-datepicker/dist/ReactHorizontalDatePicker.css';
 import * as Cookies from '../hooks/cookies';
@@ -56,9 +56,11 @@ const MyApp = ({ Component, pageProps }) => {
   }, [currentLanguageCode]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <Suspense fallback="Loading...">
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Suspense>
   );
 };
 
